79d0f96654c9ecbc64346d2c6237c081ef0f5373
MA21M <91563635+MA21M@users.noreply.github.com>
2021-12-13 20:35:37 -0500

    first

---
+++ app.js
@@ -0,0 +1,7 @@
+//imports
+const express = require('express')
+const app = express()
+const port = 3000
+// listen on port 3000
+app.listen(port, () => {console.log(`App listening on port ${port}`))
+
